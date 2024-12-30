d918f67a8739becf837c7495b36cfb73d8bd4e69
jeda-williams <jeda.williams@stmsaints.com>
2024-12-29 23:43:58 -0600

    v1

---
+++ javascript /index.js
@@ -0,0 +1,17 @@
+<header>
+
+    <div class="container-1">
+        <div class="box" class="divnav">
+            <a href="index.html" class="nav-link">Home</a>
+        </div>
+        <div class="box" class="divnav">
+            <a href="about.html" class="nav-link">About</a>
+        </div>
+        <div class="box" class="divnav">
+            <a href="about.html" class="nav-link">Contact</a>
+        </div>
+        <div class="box" class="divnav">
+            <a href="" class="nav-link">Links</a>
+        </div>
+    </div>
+  </header>
